6cdca324ce0f3b4eb71c4684a3f59dc08d6a92b7
israelswf <israelswf@gmail.com>
2020-07-20 23:46:24 -0300

    Add system test cases for New Reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,9 @@ class Reservation < ApplicationRecord
   validates_presence_of :start_date, :end_date, :guest_name, :number_of_guests
   validates_numericality_of :number_of_guests, greater_than: 0, less_than_or_equal_to: 10
   validate :start_date_is_before_end_date
+  validate :room_is_already_booked
+  validate :exceeded_room_capacity
+
 
   def duration
     if start_date.present? && end_date.present? && end_date > start_date
@@ -18,11 +21,26 @@ class Reservation < ApplicationRecord
     end
   end
 
-  private
-
   def start_date_is_before_end_date
     if start_date.present? && end_date.present? && start_date >= end_date
       errors.add(:base, :invalid_dates, message: 'The start date should be before the end date')
     end
   end
+
+  def room_is_already_booked
+    already_booked = Reservation.where("start_date < ? AND end_date > ? AND room_id = ?", end_date, start_date, room_id).present?
+    if already_booked
+      errors.add(:base, :room_already_booked, message: "Room is already booked during this period.")
+    end
+  end
+
+  def exceeded_room_capacity
+    if room_id.present?
+      room = Room.find(room_id)
+      if number_of_guests > room.capacity
+        errors.add(:base, :exceeded_room_capacity, message: "Number of guests exceeded room capacity of #{room.capacity}.")
+      end
+    end
+  end
+  
 end
--- spec/system/reservations_spec.rb
+++ spec/system/reservations_spec.rb
@@ -39,7 +39,6 @@ RSpec.describe 'Reservations', type: :system do
       end
     end
 
-=begin
     context 'when all existent rooms are already booked for given period' do
 
       before do
@@ -59,14 +58,92 @@ RSpec.describe 'Reservations', type: :system do
 
       it 'shows "There are no available rooms for the selected filters"' do
         visit search_reservations_path
-        fill_in 'From', with: '2020-08-02'
-        fill_in 'To', with: '2020-08-10'
+        fill_in 'From', with: '08/02/2020'
+        fill_in 'To', with: '08/10/2020'
         select '1', from: '# of guests'
         click_on 'Search for Available Rooms'
         expect(page).to have_content("There are no available rooms for the selected filters")
       end
     end
-=end
 
   end
+
+  describe 'creating reservation' do
+
+    room_capacity = 5
+
+    before do
+      @room = Room.create!(
+        code: '105',
+        capacity: room_capacity,
+        notes: 'Sparkling clean',
+      )
+    end
+
+    context 'when trying to create reservation with exceeding number of guests' do
+      it 'should return the error Number of guests exceeded room capacity of 5' do
+        visit new_reservation_path( reservation: {
+          room_id: 1,
+          start_date: '2020-08-02',
+          end_date: '2020-08-10',
+          number_of_guests: room_capacity,
+        }) 
+
+        fill_in 'reservation_guest_name', with: 'Bilbo Bolseiro'
+        #changing select box to 8 guests
+        select '8', from: 'reservation_number_of_guests'
+
+        click_on 'Create Reservation'
+        expect(page).to have_content("Number of guests exceeded room capacity of #{room_capacity}")
+      end
+    end
+
+    context 'when room is reserved from 2020-08-02 to 2020-08-04' do
+      before(:each) do
+        @room.reservations.destroy_all
+
+        @room.reservations.create(
+          id: 1,
+          start_date: '2020-08-02',
+          end_date: '2020-08-04',
+          guest_name: 'Carolina dos Anjos',
+          number_of_guests: 3,
+        )
+
+        visit new_reservation_path( reservation: {
+          room_id: 1,
+          start_date: '2020-08-12',
+          end_date: '2020-08-20',
+          number_of_guests: room_capacity,
+        })
+
+      end
+
+      it 'should be possible to create a reservation from 2020-08-04 to 2020-08-08' do
+        fill_in 'reservation_start_date', with: '08/04/2020'
+        fill_in 'reservation_end_date', with: '08/08/2020'
+        fill_in 'reservation_guest_name', with: 'Bilbo Bolseiro'
+        click_on 'Create Reservation'
+        expect(page).to have_content("successfully created")
+      end
+
+      it 'should be possible to create a reservation from 2020-07-30 to 2020-08-02' do
+        fill_in 'reservation_start_date', with: '07/30/2020'
+        fill_in 'reservation_end_date', with: '08/02/2020'
+        fill_in 'reservation_guest_name', with: 'Bilbo Bolseiro'
+        click_on 'Create Reservation'
+        expect(page).to have_content("successfully created")
+      end
+
+      it 'should not be possible to create a reservation from 2020-08-01 to 2020-08-03' do
+        fill_in 'reservation_start_date', with: '08/01/2020'
+        fill_in 'reservation_end_date', with: '08/03/2020'
+        fill_in 'reservation_guest_name', with: 'Bilbo Bolseiro'
+        click_on 'Create Reservation'
+        expect(page).to have_content("Room is already booked during this period")
+      end
+
+    end
+  end
+
 end
